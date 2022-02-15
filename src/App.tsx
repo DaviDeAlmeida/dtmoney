d6ec06a18a88b7c012fab80dcb02e4f20615e3bf
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-14 23:43:28 -0300

    added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+`
 
 export function App() { // Quem import não consegue mudar o nome sem usar o "as", autocomplete mias inteligente
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>HelloWorld</Title>
+      
     </div>
   );
 }
