48b3f75a742458e67b4d34752dca83d69f91c185
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-16 01:11:57 -0300

    added summary and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
@@ -6,6 +7,7 @@ export function App() { // Quem import não consegue mudar o nome sem usar o "as
     <div className="App">
       <>
         <Header />
+        <Dashboard />
         <GlobalStyle />
       </>
     </div>
