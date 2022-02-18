aad742a31fc1bdd6627cd3605cb5f476896affc4
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-17 22:40:35 -0300

    chield component receive function from parent to state manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() { // Quem import não consegue mudar o nome sem usar o "as", autocomplete mias inteligente
+  const [newTransactionModalIsOpen, setNewTransactionModalIsOpen] = useState(false);
+
+    //quando a função é executada a partir de uma ação do usuário, usar handle como prefixo
+    function handleOpenNewTransactionModal(){
+        setNewTransactionModalIsOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setNewTransactionModalIsOpen(false);
+    }
+
   return (
     <div className="App">
       <>
-        <Header />
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
         <Dashboard />
+        <Modal 
+            isOpen={newTransactionModalIsOpen}
+            onRequestClose={handleCloseNewTransactionModal}
+        >
+        <h2>Cadastrar transação</h2>
+        </Modal>
         <GlobalStyle />
       </>
     </div>
