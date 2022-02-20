525c501ed7500fa1ed599f23f0e8b967c4ab13b6
Davi Cardoso <davi.almeida.c@gmail.com>
2022-02-20 20:13:59 -0300

    added createTransaction inside TransactionsProvider

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useContext } from "react";
 import { Container, TransactioTypeContainer, RadioBox } from "./styles";
+import { TransactionsContext } from "../../TransactionsContext";
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,22 +12,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+
+   const { createTransaction } = useContext(TransactionsContext);
+
    const [type, setType] = useState('deposit');
    const [title, setTitle] = useState('')
-   const [value, setValue] = useState(0)
+   const [amount, setAmount] = useState(0)
    const [category, setCategory] = useState('')
 
    function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
-        title,
-        value,
-        category,
-        type,
-    };
-
-    api.post('/transactions', data);
+   createTransaction({
+       title,
+       amount,
+       category,
+       type
+   })
    }
 
     return (
@@ -57,8 +58,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                     <input 
                         type="number" 
                         placeholder="Valor" 
-                        value={value}
-                        onChange={event => setValue(Number(event.target.value))}
+                        value={amount}
+                        onChange={event => setAmount(Number(event.target.value))}
                     />
 
                     <TransactioTypeContainer>
